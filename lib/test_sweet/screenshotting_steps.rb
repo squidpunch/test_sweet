9dfd42a34b420b4bd7062a89575a5dd82bae0dcb
David Larrabee <david.larrabee@meyouhealth.com>
2015-09-17 13:03:06 -0400

    Add ability to include max attempts and fuzz factor for defined steps

--- lib/test_sweet/screenshotting_steps.rb
+++ lib/test_sweet/screenshotting_steps.rb
@@ -1,6 +1,14 @@
-Then /^the screen should match "([^\"]*)"$/ do |template|
-  screenshotter = Juxtapose::Screenshotter.new(self, template, 0, File.expand_path('./'))
+Then /^the screen should match "([^\"]*)" with fuzz factor of (\d+)$/ do |template, fuzz|
+  step %~the screen should match "#{template}" with fuzz factor of #{fuzz} with 10 max attempts~
+end
 
-  max_attempts = 10
-  expect(screenshotter.attempt_verify(max_attempts)).to eq(true)
+Then /^the screen should match "([^\"]*)"$/ do |template|
+  step %~the screen should match "#{template}" with fuzz factor of 0 with 10 max attempts~
+end
+
+Then(/^the screen should match "([^\"]*)" with fuzz factor of (\d+) with (\d+) max attempts$/) do |name, fuzz, max_attempts|
+
+  screenshotter = Juxtapose::Screenshotter.new(self, name, fuzz.to_i, File.expand_path('./'))
+
+  expect(screenshotter.attempt_verify(max_attempts.to_i)).to eq(true)
 end
